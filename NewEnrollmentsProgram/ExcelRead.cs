14c2bb1abb7d76ec9c8a1a3b4578a5897918f25b
snchvz <43590373+snchvz@users.noreply.github.com>
2019-08-15 15:13:47 -0700

    importing Proj

---
+++ NewEnrollmentsProgram/ExcelRead.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using EXL = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace NewEnrollmentsProgram
+{
+    public class ExcelRead
+    {
+        string path = "";
+
+        int count = 0;
+
+        _Application excel = new EXL.Application();
+
+        _Application excelDest = new EXL.Application();
+
+
+        public ExcelRead(string path, int sheet)
+        {
+
+        }
+
+        public int readWriteCell(int monthInt, string year, string path, int sheet, string filename, string destFilename)
+        {
+            int id1 = 0;
+            int id2 = 0;
+
+            this.path = path;
+
+            var wbs = excel.Workbooks;
+            var wb = wbs.Open(path);
+            var sheets = wb.Worksheets;
+            var ws = excel.Sheets[1];
+
+            var workbooksDest = excelDest.Workbooks;
+            var wbDest = workbooksDest.Open(destFilename);
+            var wsDest = excelDest.Worksheets[1];
+
+            try
+            {
+                var processes = Process.GetProcessesByName("excel");
+                id1 = processes[0].Id;
+                id2 = processes[1].Id;
+            }
+            catch
+            {
+                return -1;
+            }                      
+            
+            string month;
+
+            switch (monthInt)
+            {
+                case 1:
+                    monthInt = 10;
+                    month = monthInt.ToString();
+                    break;
+                case 2:
+                    monthInt = 11;
+                    month = monthInt.ToString();
+                    break;
+                case 3:
+                    monthInt = 12;
+                    month = monthInt.ToString();
+                    break;
+                default:
+                    monthInt -= 3;
+                    month = monthInt.ToString();
+                    break;
+            }
+
+            try
+            {
+                foreach (dynamic worksheet in wbDest.Worksheets)
+                {
+                    worksheet.Cells.ClearContents();
+                }
+
+                wbDest.Save();
+            }
+            finally
+            {
+
+            }
+
+            int hireCol = 15;
+            int termCol = 18;
+            int reHireCol = 16;
+            int EEIDCol = 1;
+            int fNameCol = 2;
+            int lNameCol = 3;
+            int deptCol = 14;
+            int posCol = 21;
+
+            int i = 2;
+
+            //ws.Cells[row, EEIDCol].Value != null
+
+            for (int row = 2; ws.Cells[row, EEIDCol].Value != null; row++)
+            {                
+                if (ws.Cells[row, hireCol].Value != null && ws.Cells[row, hireCol].Value.GetType() == typeof(DateTime))
+                {
+                    if (ws.Cells[row, hireCol].Value.Month.ToString() == month && ws.Cells[row, hireCol].Value.Year.ToString() == year)
+                    {
+                        if (ws.Cells[row, termCol].Value != null)
+                        {
+                            if (ws.Cells[row, termCol].Value.ToString() != "T")
+                            {
+                                wsDest.Cells[i, 1].Value = ws.Cells[row, EEIDCol].Value;
+                                wsDest.Cells[i, 2].Value = ws.Cells[row, deptCol].Value;
+                                wsDest.Cells[i, 3].Value = ws.Cells[row, fNameCol].Value;
+                                wsDest.Cells[i, 4].Value = ws.Cells[row, lNameCol].Value;
+                                wsDest.Cells[i, 5].Value = ws.Cells[row, hireCol].Value;
+                                wsDest.Cells[i, 6].Value = ws.Cells[row, reHireCol].Value;
+                                wsDest.Cells[i, 7].Value = ws.Cells[row, posCol].Value;
+
+                                i++;
+                            }
+                        }
+                        else
+                        {
+                            wsDest.Cells[i, 1].Value = ws.Cells[row, EEIDCol].Value;
+                            wsDest.Cells[i, 2].Value = ws.Cells[row, deptCol].Value;
+                            wsDest.Cells[i, 3].Value = ws.Cells[row, fNameCol].Value;
+                            wsDest.Cells[i, 4].Value = ws.Cells[row, lNameCol].Value;
+                            wsDest.Cells[i, 5].Value = ws.Cells[row, hireCol].Value;
+                            wsDest.Cells[i, 6].Value = ws.Cells[row, reHireCol].Value;
+                            wsDest.Cells[i, 7].Value = ws.Cells[row, posCol].Value;
+
+                            i++;
+                        }
+
+                    }                    
+                }
+            }
+
+            wbDest.Save();
+
+            wb.Close();
+            wbDest.Close();
+
+            excel.Quit();
+            excelDest.Quit();
+
+            Marshal.ReleaseComObject(sheets);
+            Marshal.ReleaseComObject(wb);
+            Marshal.ReleaseComObject(wbs);
+            Marshal.ReleaseComObject(excel);
+
+            Marshal.ReleaseComObject(wbDest);
+            Marshal.ReleaseComObject(workbooksDest);
+            Marshal.ReleaseComObject(wsDest);
+            Marshal.ReleaseComObject(excelDest);
+
+            try
+            {
+                Process.GetProcessById(id1).Kill();
+                Process.GetProcessById(id2).Kill();
+            }
+            catch
+            {
+                return -1;
+            }
+
+            return count;            
+        }
+
+        private void KillExcelProcesses()
+        {
+            var processes = Process.GetProcessesByName("excel");
+
+            foreach (Process p in processes)
+                p.Kill();
+
+            //foreach (var process in processes)
+            //{
+            //    string p = process.;
+            //    if (process.proc == excelFileName + " - Microsoft Excel" )
+            //        process.Kill();
+            //}
+        }
+    }
+}
---
+++ NewEnrollmentsProgram/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace NewEnrollmentsProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MonthComboBox.Items.Add("1");
+            MonthComboBox.Items.Add("2");
+            MonthComboBox.Items.Add("3");
+            MonthComboBox.Items.Add("4");
+            MonthComboBox.Items.Add("5");
+            MonthComboBox.Items.Add("6");
+            MonthComboBox.Items.Add("7");
+            MonthComboBox.Items.Add("8");
+            MonthComboBox.Items.Add("9");
+            MonthComboBox.Items.Add("10");
+            MonthComboBox.Items.Add("11");
+            MonthComboBox.Items.Add("12");
+        }
+
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        OpenFileDialog ofdDest = new OpenFileDialog();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ofd.Filter = "xls|*.xlsm;*.xlsx";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+                //textBox2.Text = ofd.SafeFileName;
+            }
+
+        }
+
+        private void Read_File_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text.Length < 3 || textBox2.Text.Length < 3)
+            {
+                System.Windows.MessageBox.Show("Please Select a roster excel File and Destination File");
+                return;
+            }
+            else if (MonthComboBox.SelectedItem == null)
+            {
+                System.Windows.MessageBox.Show("Please select month of hire" );
+                return;
+            }
+
+            else if (YearTextBox.Text.Length != 4)
+            {
+                System.Windows.MessageBox.Show("Please enter an a valid year (YYYY)");
+                return;
+            }
+
+            int month = int.Parse(MonthComboBox.SelectedItem.ToString());
+
+
+            ExcelRead XL = new ExcelRead(ofd.FileName, 1);
+            System.Windows.MessageBox.Show(XL.readWriteCell(month, YearTextBox.Text, ofd.FileName, 1, ofd.SafeFileName, textBox2.Text).ToString());            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ofdDest.Filter = "xls|*.xlsm;*.xlsx";
+            if (ofdDest.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //textBox1.Text = ofd.FileName;
+                textBox2.Text = ofdDest.FileName;
+            }
+        }
+    }
+}
